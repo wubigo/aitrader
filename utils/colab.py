aa929a5078e362f8e83bc709c1237303fb8a1e63
wubigo <fn@wubigo.com>
2026-04-08 13:41:28 +0800

    updated from colab

--- utils/colab.py
+++ utils/colab.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 import IPython
 
 
@@ -12,7 +13,7 @@ def is_colab():
 
 import IPython
 
-def safe_download(filepath):
+def safe_download(filepath, lpath="/"):
     """兼容 Colab 单元格 / 脚本 / 本地环境的文件下载"""
     if not os.path.exists(filepath):
         print(f"⚠️ 文件不存在: {filepath}")
@@ -22,7 +23,7 @@ def safe_download(filepath):
         # 仅在交互式 Notebook 环境中触发浏览器下载
         if IPython.get_ipython() is not None:
             from google.colab import files
-            files.download(filepath)
+            files.download(filepath, lpath=f"{lpath}/filepath")
             return
     except Exception as e:
         print(f"ℹ️ 无法触发自动下载: {e}")
@@ -36,6 +37,15 @@ def safe_download(filepath):
 
 IN_COLAB = is_colab()
 
+
+# 示例
+data = {'姓名': ['A', 'B', 'C', 'D', 'E', 'F'],
+        '分数': [80, 95, 70, 90, 85, 100]}
+df = pd.DataFrame(data)
+
 if IN_COLAB:
     from google.colab import files
-    safe_download("ic_2021.csv.c")
+    safe_download("ic_2021.csv.c", lpath="/")
+    from google.colab import drive
+    drive.mount('/content/drive')
+    df.to_csv('/content/drive/ic_2021.csv', index=False)

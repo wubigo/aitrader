5fa7d8efce1a70210e91fa05877ba35d631bc6aa
wubigo <fn@wubigo.com>
2026-04-15 22:09:42 +0800

    ic合约更新到2015-04-16

---
+++ tq/main_con_list.py
@@ -0,0 +1,38 @@
+import os
+from datetime import date
+
+from tqsdk import TqApi, TqAuth
+from utils.backtest_logger import backup_dataframe
+
+# 从环境变量获取账号密码
+token = os.getenv("TQ_ID")
+pa = os.getenv("TQ_PASS")
+
+# 创建 API
+api = TqApi(auth=TqAuth(token, pa))
+
+# 获取行情
+#quote = api.get_quote("SHFE.ni2206")
+# quote = api.get_quote("CFFEX.IC0")
+# print(quote.last_price, quote.volume)
+#
+# # ✅ 关键：必须关闭 API，否则一定会出你那个报错
+# api.close()
+
+symbol = "CFFEX.IC2606"
+# 推荐用 with 自动关闭，
+# with TqApi(auth=TqAuth(token, pa)) as api:
+    # 正确：中金所 中证500股指期货 主力连续
+quote = api.get_quote(symbol)
+# quote = api.get_quote("CFFEX.IC主连")
+print("最新价：", quote.last_price)
+print("成交量：", quote.volume)
+
+d = api.query_his_cont_quotes(['KQ.m@CFFEX.IC'], n=2700)
+print(d)
+d.to_csv('./date-ic-all.csv')
+
+
+
+
+api.close()
